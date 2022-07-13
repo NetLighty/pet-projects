ea5940c5e429fed70629cb34546e31c33c38c7d3
NetLighty <nature.netlight@gmail.com>
2022-07-14 01:02:33 +0300

    feat: change url for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '87145b3f55db4c2282601a3d1e53cc5f',
         });
     }
