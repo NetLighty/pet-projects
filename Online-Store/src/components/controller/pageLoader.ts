c6d502130d9052740a62ee32dacf29667134945d
NetLighty <nature.netlight@gmail.com>
2022-07-18 22:23:55 +0300

    feat: add sorting

---
+++ Online-Store/src/components/controller/pageLoader.ts
@@ -0,0 +1,16 @@
+import Cart from "../cart/cart";
+import Filter from "../filters/filters";
+import dakimakuras from "../itemsList/itemsDB";
+import ItemsList from "../itemsList/itemsList";
+import Sort from "../sort/sort";
+
+export class PageLoader {
+  static loadPage() {
+    ItemsList.fillPage(dakimakuras);
+    Sort.setEventListeners();
+    Filter.setColorsEventListeners();
+    Filter.setSelectedColors();
+  }
+}
+
+export default PageLoader;
