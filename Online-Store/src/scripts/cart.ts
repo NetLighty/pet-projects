064f4292e6a4545d1b4d9adec9a0d86ebc967f95
NetLighty <nature.netlight@gmail.com>
2022-08-02 19:43:46 +0300

    refactor: shorten if statements, add alert check

--- Online-Store/src/scripts/cart.ts
+++ Online-Store/src/scripts/cart.ts
@@ -9,21 +9,23 @@ export class Cart {
         const selectedDakimakura = document.getElementById(dakimakura.name);
         const selectedBookmark =
           selectedDakimakura?.querySelector(".bookmark_selected");
-        if (selectedBookmark !== null && selectedBookmark !== undefined)
+        if (selectedBookmark)
           selectedBookmark.classList.add("selected");
       }
     });
     const counter = document.querySelector(".counter-number");
-    if (counter !== undefined && counter !== null)
+    if (counter)
       counter.innerHTML = `${Number(window.localStorage.getItem("counter"))}`;
     return selectedItemsId;
   }
 
   static sendAlert() {
     const alert = document.querySelector(".over20-alert");
-    if (!alert?.classList.contains("alert-active")) {
-      alert?.classList.add("alert-active");
-      window.setTimeout(() => alert?.classList.remove("alert-active"), 5000);
+    if (alert) {
+      if (!alert.classList.contains("alert-active")) {
+        alert.classList.add("alert-active");
+        window.setTimeout(() => alert?.classList.remove("alert-active"), 5000);
+      }
     }
   }
 }
